0313e4f4b6a1fcf43b56d8e15b078d4c5f999307
Sven Willner <sven.willner@gmail.com>
2017-08-10 23:24:20 +0200

    cmake adjusted

--- cmake/FindYAML_CPP.cmake
+++ cmake/FindYAML_CPP.cmake
@@ -7,11 +7,12 @@ find_library(YAML_CPP_LIBRARY NAMES yaml-cpp)
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(YAML_CPP DEFAULT_MSG YAML_CPP_INCLUDE_DIR YAML_CPP_LIBRARY)
+
 mark_as_advanced(YAML_CPP_INCLUDE_DIR YAML_CPP_LIBRARY)
 
-if(YAML_CPP_FOUND AND NOT TARGET YAML_CPP::yaml-cpp)
-  add_library(YAML_CPP::yaml-cpp UNKNOWN IMPORTED)
-  set_target_properties(YAML_CPP::yaml-cpp PROPERTIES INTERFACE_INCLUDE_DIRECTORIES "${YAML_CPP_INCLUDE_DIR}")
-  set_target_properties(YAML_CPP::yaml-cpp PROPERTIES IMPORTED_LINK_INTERFACE_LANGUAGES "CXX")
-  set_target_properties(YAML_CPP::yaml-cpp PROPERTIES IMPORTED_LOCATION "${YAML_CPP_LIBRARY}")
+if(YAML_CPP_FOUND AND NOT TARGET yaml-cpp)
+  add_library(yaml-cpp UNKNOWN IMPORTED)
+  set_target_properties(yaml-cpp PROPERTIES INTERFACE_INCLUDE_DIRECTORIES "${YAML_CPP_INCLUDE_DIRS}")
+  set_target_properties(yaml-cpp PROPERTIES IMPORTED_LINK_INTERFACE_LANGUAGES "CXX")
+  set_target_properties(yaml-cpp PROPERTIES IMPORTED_LOCATION "${YAML_CPP_LIBRARIES}")
 endif()
